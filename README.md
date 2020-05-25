a4ae5bd57b109e2d2c23918f4f06531abc000893
server-html <AlexNormal29@gmail.com>
2020-05-25 22:47:08 +0300

    good

--- README.md
+++
@@ -1,1 +0,0 @@
-# server-html.github.io
---
+++ js/cod-javascript.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    function toggleStyle() {
+        // Get the DOM reference
+        var Classstyle = document.getElementsByClassName("div_my_menu");
+        // Toggle 
+        Classstyle.style.display == "flex" ?  Classstyle.style.display = "block" : 
+        Classstyle.style.display = "flex"; 
+      }
+    $("button.navbar-toggler").click(function(event){
+        
+        var div_my_menu = $(".div_my_menu").css('display');
+        var telefon1 = $(".telefon1").css('margin-left');
+         // Toggle 
+        //  console.log(Classstyle);
+
+       if(div_my_menu == "flex"){
+        $('.div_my_menu').css('display', 'block') 
+        return;
+       }
+       //Функция задерживающая на 1 сек (1000мсек) возвращение стиля - display: flex;
+       else{
+        setTimeout( function(){ $('.div_my_menu').css('display', 'flex');},1000);
+       }
+   
+        telefon1 == "45px" ?  $('.telefon1').css('margin-left', '0') : $('.margin-left').css('display', '45px'); 
+        //  console.log(Classstyle);
+        
+    })
+});
+
+// $( "#clickme" ).click(function() {
+//     $( "#book" ).toggle( "slow", function() {
+//       // Animation complete.
+//     });
+//   });
+
+// function toggle_visibility(id) {
+//     var e = document.getElementById(id);
+//     if(e.style.display == 'block')
+//        e.style.display = 'none';
+//     else
+//        e.style.display = 'block';
+//  }
