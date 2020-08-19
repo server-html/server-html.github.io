3399226dc29f4f22842ceeb1edc9465fbcb73671
server-html <AlexNormal29@gmail.com>
2020-08-19 19:41:32 +0300

    sus

--- js/script.js
+++ js/script.js
@@ -1,34 +1,40 @@
 $(document).ready(function(){
+
   $('.header__burger').click(function(event){
+     // Блокировка прокрутки при открытом меню
+     $('body').toggleClass('lock');
+
     $('.header__menu').css({'opacity' : '1.0'})
     $('.header__burger, .header__menu, .iphone-off, .menu__item').toggleClass('active');
 
-    // Блокировка прокрутки при открытом меню
-    $('body').toggleClass('lock');
+   
   }); 
 
   // Ссылки меню для перехода по странице
-  $('a[href^="#"]').click(function(){
+     $('a[href^="#"]').click(function(){
     var target = $(this).attr('href');
     $('html, body').animate({scrollTop: $(target).offset().top -  $(".header__body").height()}, 500);
     // проверим существования класса active у элемента
- if ($('.header__burger').hasClass('active')) {
+     if ($('.header__burger').hasClass('active')) {
 
-  $( ".header__menu" ).animate({
+       // Разблокировать прокрутки при закрытии меню
+         $('body').toggleClass('lock');
+
+        $( ".header__menu" ).animate({
    
-    opacity: 0.0 // прозрачность элемента
+         opacity: 0.0 // прозрачность элемента
 
-  }, 500, "linear");
-  $('.header__burger, .header__menu, .iphone-off, .menu__item').toggleClass('active');
+        }, 500, "linear");
+        $('.header__burger, .header__menu, .iphone-off, .menu__item').toggleClass('active');
 
-  } else {
-       return false;
-      }
+         } else {
+         return false;
+        }
 
   // var target = $(this).attr('href');
   // $('html, body').animate({scrollTop: $(target).offset().top -  $(".header__body").height()}, 500);
   // return false;
-});
+     });
 
   // $('.menu__item').click(function(event){
 
